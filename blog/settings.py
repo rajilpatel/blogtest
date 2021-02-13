a5d8cd6a88254556f66695cfce14dcdaed8639bd
rajilpatel <rajilpatel001@gmail.com>
2021-02-13 08:39:34 +0530

    another pylance.tech in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '143.110.185.210','www.pylance.tech','pylance.tech']
+ALLOWED_HOSTS = ['127.0.0.1', '143.110.185.210','www.pylance.tech','pylance.tech','pylance.tech']
 
 
 # Application definition
